c92f9bef55cec9649c89639f210dffb6058cfc2e
Rajeev <sivarajeev511@gmail.com>
2026-02-19 23:59:46 +0530

    Log basename and extension in home .js

--- home .js
+++ home .js
@@ -1,2 +1,13 @@
 //Let's practice with paths in Node.js!
 const path = require('path');
+
+//Join the current directory with 'home' to create a path
+const homePath = path.join(__dirname, 'home');
+console.log('Home Path:', homePath);
+
+//Get the base name of the home path
+const baseName = path.basename(homePath);
+console.log('Base Name:', baseName);
+
+//Get the extension (likely empty for 'home')
+console.log('Extension:', path.extname(homePath));
